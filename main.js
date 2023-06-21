1cf55bc93b276b5d7c793e33b8a6a1b0ce895495
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2023-06-20 23:42:10 -0300

    add functions and events

--- main.js
+++ main.js
@@ -1,20 +1,57 @@
-const randomNumber = Math.round(Math.random() * 10);
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const howToPlay = document.querySelector("#howToPlay");
 
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-function handleClick(event) {
-  event.preventDefault();
-  const inputNumber = document.querySelector("#inputNumber").value;
+// EVENTOS
 
-  if (Number(inputNumber) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
-    document.querySelector(
-      "#attempts"
-    ).innerText = `Acertou em ${xAttempts} vezes`;
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", pressEnterToReset)
+
+// FUNÇÕES
+
+function handleTryClick(e) {
+  e.preventDefault();
+  const inputNumber = document.querySelector("#inputNumber");
+  howToPlay.classList.add("hide")
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    document
+      .querySelector("#attempts")
+      .innerText = `Acertou em ${xAttempts} vezes`;
   }
 
-  xAttempts++;
+  if(inputNumber.value >= 0 && inputNumber.value <= 10) {
+    inputNumber.value = ""
+    xAttempts++;
+  }else{
+    howToPlay.classList.remove("hide")
+  }
 
-  console.log(xAttempts);
 }
+
+function handleResetClick(e) {
+  toggleScreen()
+
+  randomNumber = Math.round(Math.random() * 10);
+  document.querySelector("#inputNumber").value = "";
+  xAttempts = 1;
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function pressEnterToReset(e) {
+  if(e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
+}
+
